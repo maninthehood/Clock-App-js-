4b1f02f1382631de8ca1ab15f192f7f958b2eabd
maninthehood <115598773+maninthehood@users.noreply.github.com>
2023-05-31 16:17:25 -0400

    2 files modified

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log('Hello!');
+const hoursEl = document.querySelector('.hour');
+const minEl = document.querySelector('.min');
+const secEl = document.querySelector('.sec');
+
+let date = new Date();
+let hours = date.getHours();
+let min = date.getMinutes();
+let sec = date.getSeconds();
+
+hoursEl.textContent = hours;
+minEl.textContent = min;
+secEl.textContent = sec;
+
+if (sec <= 0) {
+  secEl.textContent += 0;
+}
+
+// setInterval(() => {
+hoursEl.textContent = hours;
+minEl.textContent = min;
+secEl.textContent = sec;
+
+if (sec <= 0) {
+  secEl.textContent += 0;
+}
+// }, 1000)
